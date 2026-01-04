11a141ce729d9a681c072714fa89e666fd2cd781
harris2001 <harrishadjiantonis@gmail.com>
2026-01-04 23:46:32 +0000

    Adding docstrings

--- src/impl/x86_64/gdt.cpp
+++ src/impl/x86_64/gdt.cpp
@@ -46,5 +46,21 @@ void GDT::set_gate(int idx, uint32_t base, uint32_t limit, uint8_t access, uint8
     entries[idx].access = access;
 }
 
+void GDT::write_tss(int idx, uint32_t ss0, uint32_t esp0)
+{
+    tss_entry* tss = reinterpret_cast<tss_entry*>(&entries[idx]);
+
+    // Clear out the TSS
+    memset(tss, 0, sizeof(tss_entry));
+
+    tss->SS0 = ss0;
+    tss->ESP0 = esp0;
+
+    uint32_t base = reinterpret_cast<uint32_t>(tss);
+    uint32_t limit = sizeof(tss_entry);
+
+    set_gate(idx, base, limit, 0x89, 0x00);  // Access byte: Present, Ring 0, TSS (0x89)
+}
+
 }  // namespace x86_64
 }  // namespace Arachne
--- src/impl/x86_64/gdt.hpp
+++ src/impl/x86_64/gdt.hpp
@@ -31,22 +31,21 @@ private:
         uint16_t base_low;
         uint8_t base_middle;
         uint8_t access;
-
         uint8_t granularity;
         uint8_t base_high;
     } __attribute__((packed));
 
     struct tss_entry {
-        uint32_t SSP;
-        uint16_t IOPB;
+        uint32_t SSP;          // Shadow Stack Pointer
+        uint16_t IOPB;         // I/O Map Base Address
         uint16_t LDTR;
-        uint16_t GS;
+        uint16_t GS;           // Extra segment GS 
         uint16_t FS;
         uint16_t DS;
         uint16_t SS;
         uint16_t CS;
         uint16_t ES;
-        uint32_t EDI;
+        uint32_t EDI;         // General purpose registers
         uint32_t ESI;
         uint32_t EBP;
         uint32_t ESP;
@@ -54,27 +53,49 @@ private:
         uint32_t EDX;
         uint32_t ECX;
         uint32_t EAX;
-        uint32_t EFLAGS;
-        uint32_t EIP;
-        uint32_t CR3;
-        uint16_t SS2;
-        uint32_t ESP2;
+        uint32_t EFLAGS;     // Processor state
+        uint32_t EIP;        // Instruction pointer
+        uint32_t CR3;        // Control Register 3 (Page Directory Base Register)
+        uint16_t SS2;        // Segment selector for Ring 2 stack
+        uint32_t ESP2;       // Stack pointer for Ring 2
         uint16_t SS1;
-        uint32_t ESP1;
-        uint16_t SS0;        // Segment selector for Ring 0 stack
-        uint32_t ESP0;
+        uint32_t ESP1;       
+        uint16_t SS0;
+        uint32_t ESP0;       
         uint16_t LINK;       // Previous Task Link field (Contains the segment selector of the previous TSS)
    } __attribute__((packed));
 
+    /*
+     * Sets a GDT entry at the specified index.
+     *
+     * @param idx Index of the GDT entry to set.
+     * @param base Base address of the segment.
+     * @param limit Limit of the segment.
+     * @param access Access flags for the segment.
+     * @param flags Additional flags for the segment.
+     */
+    void set_gate(int idx, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags);
+
+    /*
+     * Loads the GDT into the CPU's GDTR register.
+     *
+     * @param gdtr_address Address of the GDT pointer structure.
+     */
+    void load_gdt(uint32_t gdtr_address);
+
+    /*
+     * Writes a Task State Segment (TSS) entry into the GDT.
+     *
+     * @param idx Index of the GDT entry to write the TSS to.
+     * @param ss0 Stack segment selector for Ring 0.
+     * @param esp0 Stack pointer for Ring 0.
+     */
+    void write_tss(int idx, uint32_t ss0, uint32_t esp0);
+    
     static const int GDT_ENTRIES = 6;  // Null, Code, Data, User Code, User Data, TSS
     gdt_entry entries[GDT_ENTRIES];
     gdt_ptr gdtr;
-
-    void set_gate(int idx, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags);
-
-    void load_gdt(uint32_t gdtr_address);
-
-    void write_tss(uint32_t idx, uint32_t ss0, uint32_t esp0);
+    
 };
 
 }  // namespace x86_64
